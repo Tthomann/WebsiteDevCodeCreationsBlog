3515b4b1ba20577722901875e79420ee5d684dc5
Tyler Thomann <Tyler Thomann>
2016-01-04 10:44:16 -0600

    homework update

---
+++ javascript.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	</head>
+	<body>
+	<script>
+	var name = 'Tyler';  
+	console.log('Tyler:', name);  
+	window.confirm ("20");
+	</script>
+	</body>
+	</html>
+	
---
+++ javascripwork.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<script src="javascript.js" type="text/javascript"></script>
+	</head>
+	<body>
+	</body>
+	</html>
+	
